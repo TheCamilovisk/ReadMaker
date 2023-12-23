87c8764c33849611d9b96480a7fa8c70d1e93cc7
TheCamilovisk <camilolgon@gmail.com>
2023-12-22 23:35:55 -0300

    Enhancing the DefaultRepositoryAdapter class.

--- src/repositoryadapters/defaultadapter.py
+++ src/repositoryadapters/defaultadapter.py
@@ -1,7 +1,7 @@
 import os
 from typing import Dict, List
 
-from src.utils.files import get_file_contents, get_files_list
+from src.utils.files import get_file_contents, get_files_list, get_relative_path
 from src.utils.repository import get_repo, get_repo_name_from_url
 
 from .baseadapter import BaseRepositoryAdapter
@@ -10,17 +10,17 @@ from .baseadapter import BaseRepositoryAdapter
 class DefaultRepositoryAdapter(BaseRepositoryAdapter):
     def __init__(self, repo_url: str, base_dir: str) -> None:
         super().__init__()
-        self._repo_url = repo_url
-        self._base_dir = base_dir
-        self._repo_name = get_repo_name_from_url(self._repo_url)
-        self._repo_path = os.path.join(base_dir, self._repo_name)
-        self._repo = get_repo(self._repo_url, self._repo_path)
+        self.repo_url = repo_url
+        self.base_dir = base_dir
+        self.repo_name = get_repo_name_from_url(self.repo_url)
+        self.repo_path = os.path.join(base_dir, self.repo_name)
+        self.repo = get_repo(self.repo_url, self.repo_path)
 
     def _get_repo_relative_paths(self, repo_list: List[str]) -> List[str]:
-        return [p.replace(self._base_dir, "")[1:] for p in repo_list]
+        return [get_relative_path(p, self.base_dir) for p in repo_list]
 
     def repo_list(self, absolute: bool = False) -> List[str]:
-        raw_files_list = get_files_list(self._repo_path, include_hidden=False)
+        raw_files_list = get_files_list(self.repo_path, include_hidden=False)
         files_list = (
             raw_files_list
             if absolute
@@ -34,7 +34,7 @@ class DefaultRepositoryAdapter(BaseRepositoryAdapter):
         for file in files_list:
             try:
                 contents = get_file_contents(file)
-                contents_map[file] = contents
+                contents_map[get_relative_path(file, self.base_dir)] = contents
             except ValueError:
                 continue
             except Exception as e:
