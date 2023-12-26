0f200dd329ba1f3698483e0c5cb63dd4420646da
TheCamilovisk <camilolgon@gmail.com>
2023-12-26 15:12:34 -0300

    Fixing the prompt texts formats.

--- src/utils/prompt.py
+++ src/utils/prompt.py
@@ -2,8 +2,15 @@ from typing import Dict, List
 
 
 def get_files_structure_text(files_list: List[str]) -> str:
-    pass
+    text = "Project file structure:\n"
+    text += "\n".join(f"\t- {file}" for file in files_list)
+    return text
 
 
 def get_files_contents_text(files_contents: Dict[str, str]) -> str:
-    pass
+    text = "Projects files contents:\n"
+    text += "\n".join(
+        f"\t- File: {file}\n\t- Contents:\n```\n{contents}\n```\n"
+        for file, contents in files_contents.items()
+    )
+    return text
