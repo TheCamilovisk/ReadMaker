bfad0be611a53e941bb48c1fa34e1c8352d5b38b
TheCamilovisk <camilolgon@gmail.com>
2023-12-26 21:04:01 -0300

    Add the generate_readme test.

--- src/tests/test_llm_models.py
+++ src/tests/test_llm_models.py
@@ -1,7 +1,10 @@
 import unittest
+from unittest.mock import patch
+
+from langchain.llms.fake import FakeListLLM
 
 from src.llmmodels.defaultllmmodel import DefaultLLMModel
-from src.utils.prompt import get_files_contents_text, get_files_structure_text
+from src.utils.prompt import get_files_summaries_text, get_files_structure_text
 
 
 class TestDefaultLLMModel(unittest.TestCase):
@@ -16,14 +19,15 @@ class TestDefaultLLMModel(unittest.TestCase):
             "TestRepo/file2.py": "File 2 contents",
         }
 
-    def test_get_prompt(self):
+    @patch("src.llmmodels.defaultllmmodel.OpenAI")
+    def test_get_prompt(self, mock_openAi):
         expected_prompt = """Generate a README.md for a repository with the following features:
 
 Project file structure:
 \t- TestRepo/file1.py
 \t- TestRepo/file2.py
 
-Projects files contents:
+Projects files contents summaries:
 \t- File: TestRepo/file1.py
 \t- Contents:
 ```
@@ -36,17 +40,28 @@ File 1 contents
 File 2 contents
 ```
 
-The README.md should provide a clear and concise overview of the repository.
-The output must be in markdown code only."""
+The README.md should provide a clear and concise overview of the repository."""
+
+        mock_openAi.result_value = FakeListLLM(responses=["foo", "bar"])
 
         file_structure_text = get_files_structure_text(self.sample_file_structure)
-        files_contents_text = get_files_contents_text(self.sample_files_contents)
+        files_contents_text = get_files_summaries_text(self.sample_files_contents)
 
         llm_model = DefaultLLMModel()
-        prompt_text = llm_model.get_prompt().format(
+        prompt_text = llm_model._get_readme_prompt().format(
             file_structure=file_structure_text, files_contents=files_contents_text
         )
         self.assertEqual(prompt_text, expected_prompt)
+
+    @patch("src.llmmodels.defaultllmmodel.OpenAI")
+    def test_generate_readme(self, mock_openAi):
+        mock_openAi.return_value = FakeListLLM(responses=["bar"])
+
+        llm_model = DefaultLLMModel()
+        reponse = llm_model.generate_readme(
+            self.sample_file_structure, self.sample_files_contents
+        )
+        self.assertEqual(type(reponse), str)
 
 
 if __name__ == "__main__":
