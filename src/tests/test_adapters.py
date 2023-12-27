ef9a9664fe06a83426fb404626c03993d1867610
TheCamilovisk <camilolgon@gmail.com>
2023-12-26 21:03:32 -0300

    Fixing the adapters tests.

--- src/tests/test_adapters.py
+++ src/tests/test_adapters.py
@@ -31,7 +31,8 @@ class TestDefaultAdapter(unittest.TestCase):
 
     @patch("src.repositoryadapters.defaultadapter.get_repo")
     @patch("src.repositoryadapters.defaultadapter.get_files_list")
-    def test_repo_list(self, mock_get_files_list, mock_get_repo):
+    @patch("src.utils.repository.get_repo_ignored")
+    def test_repo_list(self, mock_get_repo_ignored, mock_get_files_list, mock_get_repo):
         expected_result1 = [
             "/home/workspace/TestRepo/file1.py",
             "/home/workspace/TestRepo/file2.py",
@@ -43,6 +44,7 @@ class TestDefaultAdapter(unittest.TestCase):
 
         mock_get_repo.return_value = "mocked repo"
         mock_get_files_list.return_value = expected_result1
+        mock_get_repo_ignored.return_value = []
 
         adapter = DefaultRepositoryAdapter(
             "https://git-provider/owner/TestRepo", "/home/workspace"
@@ -54,8 +56,13 @@ class TestDefaultAdapter(unittest.TestCase):
     @patch("src.repositoryadapters.defaultadapter.get_repo")
     @patch("src.repositoryadapters.defaultadapter.get_files_list")
     @patch("src.repositoryadapters.defaultadapter.get_file_contents")
+    @patch("src.utils.repository.get_repo_ignored")
     def test_repo_files_contents(
-        self, mock_get_file_contents, mock_get_files_list, mock_get_repo
+        self,
+        mock_get_repo_ignored,
+        mock_get_file_contents,
+        mock_get_files_list,
+        mock_get_repo,
     ):
         files_contents = {
             "/home/workspace/TestRepo/file1.py": "File 1 contents",
@@ -74,6 +81,7 @@ class TestDefaultAdapter(unittest.TestCase):
             "/home/workspace/TestRepo/file2.py",
         ]
         mock_get_file_contents.side_effect = get_file_contents_side_effect
+        mock_get_repo_ignored.return_value = []
 
         adapter = DefaultRepositoryAdapter(
             "https://git-provider/owner/TestRepo", "/home/workspace"
