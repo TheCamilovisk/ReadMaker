3ce4ea3b6d86ff76f361d7730058a2ec1f716448
TheCamilovisk <camilolgon@gmail.com>
2023-12-26 21:01:34 -0300

    Removing unnecessary inputs and methods.

--- src/llmmodels/basellmmodel.py
+++ src/llmmodels/basellmmodel.py
@@ -1,13 +1,6 @@
-import abc
-from typing import Dict, List
-from langchain.prompts import BasePromptTemplate
+from typing import Any, Protocol
 
 
-class BaseLLMModel(abc.ABC):
-    def get_prompt(self) -> BasePromptTemplate:
-        pass
-
-    def generate_readme(
-        self, files_structure: List[str], files_contents: Dict[str, str]
-    ) -> str:
+class BaseLLMModel(Protocol):
+    def generate_readme(self, **kwargs: Any) -> str:
         pass
