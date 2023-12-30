7ace5826633d005efc892e535f8cacdeed12a21b
TheCamilovisk <camilolgon@gmail.com>
2023-12-30 16:52:11 -0300

    Refining the text outputs.

--- src/utils/prompt.py
+++ src/utils/prompt.py
@@ -5,14 +5,14 @@ from langchain.chains.base import Chain
 
 def get_files_structure_text(files_list: List[str]) -> str:
     text = "Project file structure:\n"
-    text += "\n".join(f"\t\t- {file}" for file in files_list)
+    text += "\n".join(f"- {file}" for file in files_list)
     return text
 
 
 def get_files_summaries_text(files_summaries: Dict[str, str]) -> str:
     text = "Projects files contents summaries:\n"
-    text += "\n".join(
-        f"\t\t- File: {file}\n\t\t- Contents: {summary}"
+    text += "\n\n".join(
+        f"- File: {file}\n- Contents: {summary}"
         for file, summary in files_summaries.items()
     )
     return text
