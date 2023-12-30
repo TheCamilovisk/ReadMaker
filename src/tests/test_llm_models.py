29cea41de62d6a90516f14c9bdc52282d97caf41
TheCamilovisk <camilolgon@gmail.com>
2023-12-30 16:56:13 -0300

    Fixing the tests.

--- src/tests/test_llm_models.py
+++ src/tests/test_llm_models.py
@@ -4,7 +4,10 @@ from unittest.mock import patch
 from langchain.llms.fake import FakeListLLM
 
 from src.llmmodels.defaultllmmodel import DefaultLLMModel
-from src.utils.prompt import get_files_summaries_text, get_files_structure_text
+from src.tests.utils import get_resource_path, get_text_resource
+from src.utils.prompt import get_files_structure_text, get_files_summaries_text
+from src.configs.llmmodels.defaultllmmodel import DefaultLLMModelConfig
+from src import config
 
 
 class TestDefaultLLMModel(unittest.TestCase):
@@ -19,49 +22,28 @@ class TestDefaultLLMModel(unittest.TestCase):
             "TestRepo/file2.py": "File 2 contents",
         }
 
-    @patch("src.llmmodels.defaultllmmodel.OpenAI")
+    @patch("src.llmmodels.defaultllmmodel.ChatOpenAI")
     def test_get_prompt(self, mock_openAi):
-        expected_prompt = """Generate a README.md for a repository with the following features:
-
-Project file structure:
-\t- TestRepo/file1.py
-\t- TestRepo/file2.py
-
-Projects files contents summaries:
-\t- File: TestRepo/file1.py
-\t- Contents:
-```
-File 1 contents
-```
-
-\t- File: TestRepo/file2.py
-\t- Contents:
-```
-File 2 contents
-```
-
-The README.md should provide a clear and concise overview of the repository."""
+        expected_prompt_path = get_resource_path("expected_prompt.txt")
+        expected_prompt = get_text_resource(expected_prompt_path)
 
         mock_openAi.result_value = FakeListLLM(responses=["foo", "bar"])
 
         file_structure_text = get_files_structure_text(self.sample_file_structure)
-        files_contents_text = get_files_summaries_text(self.sample_files_contents)
+        files_summaries_text = get_files_summaries_text(self.sample_files_contents)
 
         llm_model = DefaultLLMModel()
-        prompt_text = llm_model._get_readme_prompt().format(
-            file_structure=file_structure_text, files_contents=files_contents_text
+        prompt_text = (
+            llm_model._get_introduction_prompt()
+            .format(
+                files_structure=file_structure_text,
+                files_summaries=files_summaries_text,
+            )
+            .replace("Human: ", "")
         )
+        print(prompt_text)
+        print(expected_prompt)
         self.assertEqual(prompt_text, expected_prompt)
-
-    @patch("src.llmmodels.defaultllmmodel.OpenAI")
-    def test_generate_readme(self, mock_openAi):
-        mock_openAi.return_value = FakeListLLM(responses=["bar"])
-
-        llm_model = DefaultLLMModel()
-        reponse = llm_model.generate_readme(
-            self.sample_file_structure, self.sample_files_contents
-        )
-        self.assertEqual(type(reponse), str)
 
 
 if __name__ == "__main__":
