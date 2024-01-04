257842dd9fc31721ca2a9695ebd62f20376578bb
TheCamilovisk <camilolgon@gmail.com>
2024-01-04 17:30:29 -0300

    Fixing test_get_prompt test.

--- src/tests/test_llm_models.py
+++ src/tests/test_llm_models.py
@@ -6,6 +6,7 @@ from langchain.llms.fake import FakeListLLM
 from src.llmmodels.defaultllmmodel import DefaultLLMModel
 from src.tests.utils import get_resource_path, get_text_resource
 from src.utils.prompt import get_files_structure_text, get_files_summaries_text
+from src.repositoryadapters.defaultadapter import DefaultRepositoryAdapter
 
 
 class TestDefaultLLMModel(unittest.TestCase):
@@ -21,16 +22,29 @@ class TestDefaultLLMModel(unittest.TestCase):
         }
 
     @patch("src.llmmodels.defaultllmmodel.ChatOpenAI")
-    def test_get_prompt(self, mock_openAi):
+    @patch("src.repositoryadapters.defaultadapter.get_repo")
+    @patch("src.repositoryadapters.defaultadapter.get_files_list")
+    @patch("src.utils.repository.get_repo_ignored")
+    def test_get_prompt(
+        self, mock_get_repo_ignored, mock_get_files_list, mock_get_repo, mock_openAi
+    ):
         expected_prompt_path = get_resource_path("expected_prompt.txt")
         expected_prompt = get_text_resource(expected_prompt_path)
 
         mock_openAi.result_value = FakeListLLM(responses=["foo", "bar"])
 
+        mock_get_repo.return_value = "mocked repo"
+        mock_get_files_list.return_value = self.sample_file_structure
+        mock_get_repo_ignored.return_value = []
+
         file_structure_text = get_files_structure_text(self.sample_file_structure)
         files_summaries_text = get_files_summaries_text(self.sample_files_contents)
 
-        llm_model = DefaultLLMModel()
+        adapter = DefaultRepositoryAdapter(
+            "https://git-provider/owner/TestRepo", "/home/workspace"
+        )
+
+        llm_model = DefaultLLMModel(adapter)
         prompt_text = (
             llm_model._get_introduction_prompt()
             .format(
@@ -39,8 +53,6 @@ class TestDefaultLLMModel(unittest.TestCase):
             )
             .replace("Human: ", "")
         )
-        print(prompt_text)
-        print(expected_prompt)
         self.assertEqual(prompt_text, expected_prompt)
 
 
